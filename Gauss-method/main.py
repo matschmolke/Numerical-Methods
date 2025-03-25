3db5b5e5ecf905a0b15c915e23b219c085aef8c5
Samson312 <michalsamsonowicz@op.pl>
2025-03-25 15:22:50 +0100

    Changes to functions invoking

--- Gauss-method/main.py
+++ Gauss-method/main.py
@@ -1,38 +1,34 @@
 import numpy as np
 from gauss import gauss_elimination_pivot
 from beam import beam_deformation
-from leontief import leontief, generate_rand_matrix_and_vector
+from leontief import leontief
 
 # Zadanie 0: Rozwiązujemy układ 3x3 metodą eliminacji Gaussa (pivoting)
 # Dane do układu 3x3
-A_data = np.array(
+A = np.array(
 [[2, -1, 1],
 [3, 3, 9],
-[3, 3, 5]], dtype=float)
+[3, 3, 5]])
 
-b_data = np.array([8, 6, 7], dtype=float)
+b = np.array([8, 6, 7])
 
 print("Rozwiązanie układu 3x3 metodą eliminacji Gaussa z pivotowaniem:")
-print(gauss_elimination_pivot(A_data, b_data))
+print(gauss_elimination_pivot(A, b))
 print("Sprawdzanie wyniku metodą linalg.solve:")
-print(np.linalg.solve(A_data, b_data))
+print(np.linalg.solve(A, b))
 
 # Zadanie 1: Otwarty model ekonomiczny Leontiefa
 num_sectors = 10
 
-A, D = generate_rand_matrix_and_vector(num_sectors)
+X = leontief(num_sectors)
 
-X = leontief(A, D)
-
-print("Zadanie 1:")
-print("Macierz A:")
-print(A)
-print("\nWektor D:")
-print(D)
+print("Zadanie 1: Otwarty model ekonomiczny Leontiefa")
 print("\nWektor X (produkcja sektorów):")
 print(X)
 
 
 # Zadanie 2: Odkształcenia belki zamocowanej jednostronnie
 print("Odkształcenia belki zamocowanej jednostronnie:")
+
 beam_deformation()
+
