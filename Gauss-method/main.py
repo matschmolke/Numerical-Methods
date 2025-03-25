b65c86b7591787de32b63b5130aa8aa63ef33ce5
Mateusz <mateuszs_2003@wp.pl>
2025-03-25 12:04:00 +0100

    Beam deformation

--- Gauss-method/main.py
+++ Gauss-method/main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Zadanie 0: Program rozwiązujący układ równań 3x3 + sprawdzenie wyników metodą np.linalg.solve.
 def gauss_elimination_pivot(A, b):
     n = len(A)
     A = A.astype(float) # Zapobiega błędom dzielenia
@@ -38,3 +40,40 @@ print("Rozwiązanie układu 3x3 metodą eliminacji Gaussa z pivotowaniem:")
 print(gauss_elimination_pivot(A_data, b_data))
 print("Sprawdzanie wyniku metodą linalg.solve:")
 print(np.linalg.solve(A_data, b_data))
+
+
+# Zadanie 2: Odkształcenia belki z jednej strony zamocowanej
+def beam_deformation():
+    n = 5  # liczba segmentów
+    k = 200  # współczynnik sztywności N\m
+    L = 10  # długość całej belki
+    l = L / n  # długość jednego segmentu belki
+
+    # Konstruujemy macierz sztywności K
+    K = np.zeros((n, n))
+    np.fill_diagonal(K, 2)
+    np.fill_diagonal(K[1:], -1)
+    np.fill_diagonal(K[:, 1:], -1)
+    K[-1, -1] = 1  # warunek brzegowy (wolny koniec)
+    K *= k / (l ** 2)
+
+    # Tworzymy wektor sił zewnętrznych działających na belkę
+    F = np.zeros(n)
+    F[-1] = -1000  # obciążenie na ostatnim węźle
+
+    # Rozwiązanie układu równań (znajdujemy wektor przemieszczeń)
+    W = gauss_elimination_pivot(K, F)
+    print("Przemieszczenia W:", W)
+
+    # Wizualizacja wyników
+    plt.figure()
+    plt.plot(np.linspace(l, L, n), W, marker='o', linestyle='-', color='blue')
+    plt.xlabel("Długość belki [m]")
+    plt.ylabel("Ugięcie [m]")
+    plt.title("Odkształcenia belki zamocowanej jednostronnie")
+    plt.grid(True)
+    plt.show()
+
+# Odkształcenia belki zamocowanej jednostronnie
+print("Odkształcenia belki zamocowanej jednostronnie:")
+beam_deformation()
