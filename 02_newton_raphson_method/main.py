81ea3a3d366037b2e80e61c6e26ad4bb546ba27b
Samson312 <michalsamsonowicz@op.pl>
2025-04-14 21:21:48 +0200

    Changes

--- 02_newton_raphson_method/main.py
+++ 02_newton_raphson_method/main.py
@@ -32,16 +32,21 @@ print(f"Rozwiązanie z sqrt: {exact}")
 
 #Zadanie 3: Układ równań nieliniowych
 
-def newton_raphson_system(F, J, X0, epsilon=1e-7, max_iter=50):
+def nonlinear_newton_raphson(F, J, X0, epsilon=1e-7, max_iter=50):
     X = np.array(X0, dtype=float)
-    history = [X.copy()]
+
+    iterations = [X.copy()]
+
     for _ in range(max_iter):
+
         d = np.linalg.solve(J(X), -F(X))
         X += d
-        history.append(X.copy())
+
+        iterations.append(X.copy()) # Zapisywanie przybliżeń do wyświetlenia
+
         if np.linalg.norm(d) < epsilon:
             break
-    return X, np.array(history)
+    return X, np.array(iterations)
 
 #Wektor funkcji
 def F(X):
@@ -60,13 +65,23 @@ def J(X):
     ])
 
 
-# Przykładowy start
+# Rozwiązanie 1
 X0 = [1.5, 1.5]
-solution, history = newton_raphson_system(F, J, X0)
+solution1, iterations1 = nonlinear_newton_raphson(F, J, X0)
 
-print("Rozwiązanie układu:", solution)
+# Rozwiązanie 2
+X0 = [1, 0.5]
+solution2, iterations2 = nonlinear_newton_raphson(F, J, X0)
 
-# Rysowanie
+# Rozwiązanie 3
+X0 = [0, -1]
+solution3, iterations3 = nonlinear_newton_raphson(F, J, X0)
+
+print("Rozwiązanie 1 układu:", solution1)
+print("Rozwiązanie 2 układu:", solution2)
+print("Rozwiązanie 3 układu:", solution3)
+
+# Wykres
 x_vals = np.linspace(-2, 2, 400)
 y_vals = np.linspace(-2, 2, 400)
 X_grid, Y_grid = np.meshgrid(x_vals, y_vals)
@@ -74,13 +89,15 @@ X_grid, Y_grid = np.meshgrid(x_vals, y_vals)
 Z1 = X_grid**2 + Y_grid**2 - 1
 Z2 = 0.5*(X_grid + 1)**2 - Y_grid
 
-plt.contour(X_grid, Y_grid, Z1, levels=[0], colors='blue', label='f1')
-plt.contour(X_grid, Y_grid, Z2, levels=[0], colors='green', label='f2')
-plt.plot(history[:, 0], history[:, 1], 'ro--', label='iteracje')
-plt.legend(['Iteracje'])
+plt.contour(X_grid, Y_grid, Z1, levels=[0], colors='blue')
+plt.contour(X_grid, Y_grid, Z2, levels=[0], colors='green')
+plt.plot(iterations1[:, 0], iterations1[:, 1], 'ro--', label='Rozwiązanie 1 (start [1.5, 1.5])')
+plt.plot(iterations2[:, 0], iterations2[:, 1], 'mo--', label='Rozwiązanie 2 (start [1, 0.5])')
+plt.plot(iterations3[:, 0], iterations3[:, 1], 'yo--', label='Rozwiązanie 3 (start [0, -1])')
 plt.title('Metoda Newtona-Raphsona dla układu równań')
 plt.xlabel('x')
 plt.ylabel('y')
+plt.legend()
 plt.grid(True)
 plt.axis('equal')
 plt.show()
