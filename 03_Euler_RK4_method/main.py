3759165a5c3bb15699ceb3ccf7d49e17aba1ac5a
Mateusz <mateuszs_2003@wp.pl>
2025-05-13 11:25:45 +0200

    bug-fix

--- 03_Euler_RK4_method/main.py
+++ 03_Euler_RK4_method/main.py
@@ -2,6 +2,6 @@ from lotka_volterra import run_comparison_lv
 from sir_model import run_comparison_sir
 from harmonic_oscillator import run_comparison_oscillator
 
-# run_comparison_lv() #1
+run_comparison_lv() #1
 run_comparison_sir() #2
-# run_comparison_oscillator() #3
+run_comparison_oscillator() #3
